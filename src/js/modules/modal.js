b8df9358d4a72c995f9dd9d04137cc26c2287ebb
Artem Balandin <artem_balandin@outlook.com>
2022-07-17 23:02:29 +0300

    some changes modal

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -4,6 +4,9 @@ const modal = document.querySelector('.modal'),
 
 function showModal() {
 	modal.classList.remove('hide');
+	document.body.style.paddingRight = `${
+		window.innerWidth - document.documentElement.clientWidth
+	}px`;
 	modal.classList.add('show', 'fadeModal');
 	document.body.style.overflow = 'hidden';
 	clearTimeout(timeoutShowModal);
@@ -13,12 +16,13 @@ function showModal() {
 function hideModal() {
 	document.body.style.overflow = '';
 	modal.classList.remove('show', 'fadeModal');
+	document.body.style.paddingRight = '';
 	modal.classList.add('hide');
 }
 
 function showModalScrollBottom() {
 	if (
-		window.pageYOffset + document.body.clientHeight >=
+		window.pageYOffset + document.documentElement.clientHeight >=
 		document.documentElement.scrollHeight
 	) {
 		showModal();
